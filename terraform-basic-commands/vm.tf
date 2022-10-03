3c68c96ce082b3fb603d3850ea0d77d39a31dc2e
Salaton <nairouasalaton@gmail.com>
2022-10-03 19:49:44 +0300

    feat: create compute engine resource

---
+++ terraform-basic-commands/vm.tf
@@ -0,0 +1,26 @@
+# This will create a virtual machine on GCP
+
+resource "google_compute_instance" "default" {
+  name                      = "terraform-introduction"
+  zone                      = "europe-west1-d"
+  machine_type              = "n1-standard-1"
+  tags                      = ["test"]
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size  = 10
+      #   type - SSD
+      type = "pd-ssd"
+    }
+    auto_delete = true
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+      # Without any arguments here, this will give the VM an external IP address
+    }
+  }
+}
