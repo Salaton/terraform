67b0e7f8da2ce472e97f482340905d65c2e052d0
Salaton <nairouasalaton@gmail.com>
2022-10-05 15:02:29 +0300

    chore: add gcp-service-list variable
    This variable will hold all the services that we need. We shall enable
    those APIs listed within this list

--- terraform-basic-commands/variables.tf
+++ terraform-basic-commands/variables.tf
@@ -6,10 +6,22 @@ variable "credentials_file" {
 
 }
 
+variable "database_user_password" {}
+
 variable "region" {
   default = "europe-west1"
 }
 
 variable "zone" {
   default = "europe-west1-d"
+}
+
+variable "gcp_service_list" {
+  description = "A list of the necessary apis needed for the project"
+  type        = list(string)
+  default = [
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "sqladmin.googleapis.com"
+  ]
 }
